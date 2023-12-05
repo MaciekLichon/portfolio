5e11094995ee4e4f3383ec42faa97ebb05bd14da
Maciej Lichoń <maciek.lichon@gmail.com>
2023-12-05 14:59:06 +0100

    add project

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -2,6 +2,17 @@ const initialState = {
   projects: [
     {
       id: '1',
+      title: 'MD Editor',
+      technologies: 'React, Redux, Typescript',
+      description: 'CRUD App. Create markdown files with live preview and save the work with localStorage.',
+      mainImage: '/images/project_backgrounds/markdown-editor.png',
+      foregroundOne: '/images/icons/document-gradient.png',
+      foregroundTwo: '',
+      appLink: 'https://macieklichon.github.io/FM_In-browser-markdown-editor/',
+      gitLink: 'https://github.com/MaciekLichon/FM_In-browser-markdown-editor',
+    },
+    {
+      id: '2',
       title: 'Path finder',
       technologies: 'JavaScript',
       description: 'Play around with four path finding algorithms and compare their efficiency in different conditions.',
@@ -12,7 +23,7 @@ const initialState = {
       gitLink: 'https://github.com/MaciekLichon/Pathfinder',
     },
     {
-      id: '2',
+      id: '3',
       title: 'Youtify',
       technologies: 'React, Redux, Bootstrap, Youtube and Spotify API',
       description: 'Find your favorite music on Youtube and save it to your Spotify playlist with only a few clicks.',
